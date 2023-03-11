24567b1d784b0f8a5bae39b21c4d004a46cab65c
BhawitBalodi <Bhawit.Bhawitbalodi@gmail.com>
2023-03-12 03:35:45 +0530

    commit

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,5 @@
 import sys
-import logging
-
+from src.logger import logging
 
 # whenever error raises , just call this particular function
 def error_message_detail(error,error_detail:sys):
@@ -21,10 +20,5 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-if __name__ == '__main__':
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info('Divide by Zero')
-        raise CustomException(e,sys)
+
         
